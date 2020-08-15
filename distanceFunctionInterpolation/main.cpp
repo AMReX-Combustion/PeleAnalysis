41b69d7cfb57b04c5563bb2d2d9b75ac0aaea609
Shufan Zou <zou00066@nike-login-0-26.local>
2020-08-14 20:17:37 -0500

    add main.cpp to Shufan branch, segmentation fault

---
+++ distanceFunctionInterpolation/main.cpp
@@ -0,0 +1,24 @@
+#include <AMReX_Array.H>
+#include <AMReX_MultiFab.H>
+#include <AMReX_EB2.H>
+#include <AMReX_EB2_IF_Base.H>
+#include <vector>
+#include <string.h>
+#include <AMReX_MultiFab.H>
+#include <AMReX_EB2_IF_Triangulated.H>
+void main()
+{
+   char* isoFile="flatplain";
+
+   amrex::EB2::TriangulatedIF Tri(isoFile);
+
+   double A[3];
+   A[0]=0;
+   A[1]=0;
+   A[2]=0;
+
+   double distance=(Tri.distanceInterpolation_->distance(A));
+
+   
+
+}
