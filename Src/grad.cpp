f62da5afa1076a0fceac0a33c988fd2728c1b0e6
Lucas Esclapez <lucas.esclapez@nrel.gov>
2022-01-31 16:12:48 -0700

    Refactor the grad exec to use MLMG.

--- Src/grad.cpp
+++ Src/grad.cpp
@@ -4,23 +4,14 @@
 #include <AMReX_ParmParse.H>
 #include <AMReX_MultiFab.H>
 #include <AMReX_DataServices.H>
-#include <WritePlotFile.H>
-#include <AppendToPlotFile.H>
-
+#include <AMReX_MultiFabUtil.H>
+#include <AMReX_PlotFileUtil.H>
 #include <AMReX_BLFort.H>
+#include <AMReX_MLMG.H>
+#include <AMReX_MLPoisson.H>
+#include <AMReX_MLABecLaplacian.H>
 
 using namespace amrex;
-
-extern "C" {
-  void pushvtog(const int* lo,  const int* hi,
-                const int* dlo, const int* dhi,
-                Real* U, const int* Ulo, const int* Uhi,
-                const int* nc);
-  void gradient(const int* lo,  const int* hi,
-                const Real* U, const int* Ulo, const int* Uhi,
-                Real* G, const int* Glo, const int* Ghi,
-                const Real* dx);
-}
 
 static
 void 
@@ -35,7 +26,7 @@ print_usage (int,
 std::string
 getFileRoot(const std::string& infile)
 {
-  vector<std::string> tokens = Tokenize(infile,std::string("/"));
+  std::vector<std::string> tokens = Tokenize(infile,std::string("/"));
   return tokens[tokens.size()-1];
 }
 
@@ -45,144 +36,225 @@ main (int   argc,
 {
   amrex::Initialize(argc,argv);
   {
-  if (argc < 2)
-    print_usage(argc,argv);
-
-  ParmParse pp;
-
-  if (pp.contains("help"))
-    print_usage(argc,argv);
-
-  std::string infile; pp.get("infile",infile);
-  DataServices::SetBatchMode();
-  Amrvis::FileType fileType(Amrvis::NEWPLT);
-  DataServices dataServices(infile, fileType);
-  if( ! dataServices.AmrDataOk()) {
-    DataServices::Dispatch(DataServices::ExitRequest, NULL);
-  }
-  AmrData& amrData = dataServices.AmrDataRef();
-
-  int finestLevel = amrData.FinestLevel();
-  pp.query("finestLevel",finestLevel);
-  int Nlev = finestLevel + 1;
-
-  std::string gradVar = "temp"; pp.query("gradVar",gradVar);
-
-  int idC = -1;
-  const Vector<std::string>& plotVarNames = amrData.PlotVarNames();
-  for (int i=0; i<plotVarNames.size(); ++i)
-  {
-    if (plotVarNames[i] == gradVar) idC = i;
-  }
-  if (idC<0) {
-    Print() << "Cannot find required data in pltfile" << std::endl;
-  }
-
-  const int idCst = 0;
-  const int nCompIn = idCst + 1;
-
-  Vector<std::string> inVarNames(nCompIn);
-  inVarNames[idCst] = plotVarNames[idC];
-  Vector<int> destFillComps(nCompIn);
-  for (int i=0; i<nCompIn; ++i) {
-    destFillComps[i] = i;
-  }
-
-  const int idGr = nCompIn;
-  const int nCompOut = idGr + BL_SPACEDIM +1 ; // 1 component stores the ||gradT||
-
-  Vector<MultiFab*> state(Nlev);
-  Vector<Geometry*> geoms(Nlev);
-  const int nGrow = 1;
-
-  RealBox rb(&(amrData.ProbLo()[0]),
-             &(amrData.ProbHi()[0]));
-  Vector<int> is_per(BL_SPACEDIM,1);
-  pp.queryarr("is_per",is_per,0,BL_SPACEDIM);
-  int coord = 0;
-
-  for (int lev=0; lev<Nlev; ++lev) {
-
-    const BoxArray ba = amrData.boxArray(lev);
-    const DistributionMapping dmap(ba);
-    state[lev] = new MultiFab(ba,dmap,nCompOut,nGrow);
-    const Vector<Real>& delta = amrData.DxLevel()[lev];
-
-    // Get input state data onto intersection ba
-    const int myNComp = 1; // gonna need this for fortran calls
-    
-    //state[lev]->setBndry(0.0); // Initialize grow cells to 0....here don't care to get grads right on c-f
-
-    Print() << "Doing FillVar at lev: " << lev << std::endl;
-    amrData.FillVar(*state[lev],lev,inVarNames,destFillComps);
-    geoms[lev] = new Geometry(amrData.ProbDomain()[lev],&rb,coord,&(is_per[0]));
-
-    // Fix up grow cells.  Use extrap for guess
-    const Box& dbox = amrData.ProbDomain()[lev];
-    for (MFIter mfi(*state[lev]); mfi.isValid(); ++mfi)
-    {
-      FArrayBox& fab = (*state[lev])[mfi];
-      const Box& box = mfi.validbox();
-      pushvtog(BL_TO_FORTRAN_BOX(box),
-               BL_TO_FORTRAN_BOX(dbox),
-               BL_TO_FORTRAN_ANYD(fab),
-               &myNComp);
+    if (argc < 2) {
+      print_usage(argc,argv);
     }
-    
-    // Fix up fine-fine and periodic
-    state[lev]->FillBoundary(geoms[lev]->periodicity());
 
-    for (MFIter mfi(*state[lev]); mfi.isValid(); ++mfi)
-    {
-      FArrayBox& fab = (*state[lev])[mfi];
-      const Box& box = mfi.validbox();
-      gradient(BL_TO_FORTRAN_BOX(box),
-               BL_TO_FORTRAN_N_ANYD(fab,0),
-               BL_TO_FORTRAN_N_ANYD(fab,idGr),
-               &(delta[0]));
+    // ---------------------------------------------------------------------
+    // Set defaults input values
+    // ---------------------------------------------------------------------
+    std::string gradVar       = "temp";
+    std::string infile        = "";  
+    int finestLevel           = 1000;
+    int nAuxVar               = 0;
+
+    // ---------------------------------------------------------------------
+    // ParmParse
+    // ---------------------------------------------------------------------
+    ParmParse pp;
+
+    if (pp.contains("help")) {
+      print_usage(argc,argv);
     }
-  }
 
-  Vector<std::string> nnames(nCompOut);
-  for (int i=0; i<nCompIn; ++i) {
-    nnames[i] = inVarNames[i];
-  }
-  nnames[idGr+0] = gradVar + "_gx";
-  nnames[idGr+1] = gradVar + "_gy";
-#if BL_SPACEDIM==3
-  nnames[idGr+2] = gradVar + "_gz";
-#endif
-  nnames[idGr+BL_SPACEDIM] = "||grad"+ gradVar+ "||";
-  bool verb=false;
-  bool appendPlotFile=false; pp.query("appendPlotFile",appendPlotFile);
+    pp.get("infile",infile);
+    pp.query("gradVar",gradVar);
+    pp.query("finestLevel",finestLevel);
 
-  if (appendPlotFile) {
-    int nStateOut = nCompOut - nCompIn;
-    Vector<MultiFab*> ostate(Nlev);
+    // Initialize DataService
+    DataServices::SetBatchMode();
+    Amrvis::FileType fileType(Amrvis::NEWPLT);
+    DataServices dataServices(infile, fileType);
+    if( ! dataServices.AmrDataOk()) {
+      DataServices::Dispatch(DataServices::ExitRequest, NULL);
+    }
+    AmrData& amrData = dataServices.AmrDataRef();
+
+    // Plotfile global infos
+    finestLevel = std::min(finestLevel,amrData.FinestLevel());
+    int Nlev = finestLevel + 1;
+    const Vector<std::string>& plotVarNames = amrData.PlotVarNames();
+    RealBox rb(&(amrData.ProbLo()[0]), 
+               &(amrData.ProbHi()[0]));
+
+    // Gradient variable
+    int idC = -1;
+    for (int i=0; i<plotVarNames.size(); ++i)
+    {
+      if (plotVarNames[i] == gradVar) idC = i;
+    }
+    if (idC<0) {
+      Print() << "Cannot find " << gradVar << " data in pltfile \n";
+    }
+
+    // Auxiliary variables
+    nAuxVar = pp.countval("Aux_Variables");
+    Vector<std::string> AuxVar(nAuxVar);
+    for(int ivar = 0; ivar < nAuxVar; ++ivar) { 
+         pp.get("Aux_Variables", AuxVar[ivar],ivar);
+    }
+
+    // ---------------------------------------------------------------------
+    // Variables index management
+    // ---------------------------------------------------------------------
+    const int idCst = 0;
+    int nCompIn = idCst + 1;
+    Vector<std::string> inVarNames(nCompIn);
+    inVarNames[idCst] = plotVarNames[idC];
+
+    if (nAuxVar>0)
+    {
+        inVarNames.resize(nCompIn+nAuxVar);
+        for (int ivar=0; ivar<nAuxVar; ++ivar) {
+            if ( amrData.StateNumber(AuxVar[ivar]) < 0 ) {
+               amrex::Abort("Unknown auxiliary variable name: "+AuxVar[ivar]);
+            }
+            inVarNames[nCompIn] = AuxVar[ivar];
+            nCompIn ++;
+        } 
+    }
+
+    Vector<int> destFillComps(nCompIn);
+    for (int i=0; i<nCompIn; ++i) {
+      destFillComps[i] = i;
+    }
+
+    const int idGr = nCompIn;
+    const int nCompOut = idGr + AMREX_SPACEDIM +1 ; // 1 component stores the ||gradT||
+
+    // Check symmetry/periodicity in given coordinate direction
+    Vector<int> sym_dir(AMREX_SPACEDIM,0);
+    pp.queryarr("sym_dir",sym_dir,0,AMREX_SPACEDIM);  
+
+    Vector<int> is_per(AMREX_SPACEDIM,1);
+    pp.queryarr("is_per",is_per,0,AMREX_SPACEDIM);
+    Print() << "Periodicity assumed for this case: ";
+    for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+        Print() << is_per[idim] << " ";
+    }
+    Print() << "\n";
+    BCRec gradVarBC;
+    for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+        gradVarBC.setLo(idim,BCType::foextrap);
+        gradVarBC.setHi(idim,BCType::foextrap);
+        if ( is_per[idim] ) {
+            gradVarBC.setLo(idim, BCType::int_dir);
+            gradVarBC.setHi(idim, BCType::int_dir);
+        }
+    }
+
+    int coord = 0;
+
+    // ---------------------------------------------------------------------
+    // Let's start the real work
+    // ---------------------------------------------------------------------
+    Vector<MultiFab> state(Nlev);
+    Vector<Geometry> geoms(Nlev);
+    Vector<BoxArray> grids(Nlev);
+    Vector<DistributionMapping> dmap(Nlev);
+    const int nGrow = 1;
+
+    // Read data on all the levels
     for (int lev=0; lev<Nlev; ++lev) {
-      const BoxArray ba = state[lev]->boxArray();
-      const DistributionMapping dm(ba);
-      ostate[lev] = new MultiFab(ba,dm,nStateOut,nGrow);
-      MultiFab::Copy(*ostate[lev],*state[lev],nCompIn,0,nStateOut,0);
-    }
-    Vector<std::string> namesOut(nStateOut);
-    for (int i=0; i<nStateOut; ++i) {
-      namesOut[i] = nnames[nCompIn+i];
+
+      const BoxArray ba = amrData.boxArray(lev);
+      grids[lev] = ba;
+      dmap[lev] = DistributionMapping(ba);
+      geoms[lev] = Geometry(amrData.ProbDomain()[lev],&rb,coord,&(is_per[0]));
+      state[lev].define(grids[lev], dmap[lev], nCompOut, nGrow);
+
+      Print() << "Reading data for level: " << lev << std::endl;
+      amrData.FillVar(state[lev], lev, inVarNames, destFillComps);
+
+      state[lev].FillBoundary(idCst,1,geoms[lev].periodicity());
     }
 
-    std::string newMFBaseName = "NEWDAT"; pp.query("newMFBaseName",newMFBaseName);
-    std::string newHeaderName = "NewHeader"; pp.query("newHeaderName",newHeaderName);
-    AppendToPlotFile(amrData,ostate,infile,namesOut,newMFBaseName,newHeaderName,verb);
+    // Get face-centered gradients from MLMG
+    LPInfo info;
+    info.setAgglomeration(1);
+    info.setConsolidation(1);
+    info.setMetricTerm(false);
+    info.setMaxCoarseningLevel(0);
+    MLPoisson poisson({geoms}, {grids}, {dmap}, info);
+    poisson.setMaxOrder(4);
+    std::array<LinOpBCType, AMREX_SPACEDIM> lo_bc;
+    std::array<LinOpBCType, AMREX_SPACEDIM> hi_bc;
+    for (int idim = 0; idim< AMREX_SPACEDIM; idim++){
+       if (is_per[idim] == 1) {
+          lo_bc[idim] = hi_bc[idim] = LinOpBCType::Periodic;
+       } else {
+          if (sym_dir[idim] == 1) {
+             lo_bc[idim] = hi_bc[idim] = LinOpBCType::reflect_odd;
+          } else {
+             lo_bc[idim] = hi_bc[idim] = LinOpBCType::Neumann;
+          }
+       }
+    }
+    poisson.setDomainBC(lo_bc, hi_bc);
 
-    Print() << "...finished.  Note: to see new data, you must rename NewHeader in the" << std::endl;
-    Print() << "              pltfile to Header (probably want to save the original first)" << std::endl;
-  } else {
+    // Need to apply the operator to ensure CF consistency with composite solve
+    int nGrowGrad = 0;                   // No need for ghost face on gradient
+    Vector<Array<MultiFab,AMREX_SPACEDIM> > grad(Nlev);
+    Vector<std::unique_ptr<MultiFab>> phi;
+    Vector<MultiFab> laps;
+    for (int lev = 0; lev < Nlev; ++lev) {
+      for (int idim = 0; idim <AMREX_SPACEDIM; idim++) {
+         const auto& ba = grids[lev];
+         grad[lev][idim].define(amrex::convert(ba,IntVect::TheDimensionVector(idim)),
+                                dmap[lev], 1, nGrowGrad);
+      }    
+      phi.push_back(std::make_unique<MultiFab> (state[lev],amrex::make_alias,idCst,1));
+      poisson.setLevelBC(lev, phi[lev].get());
+      laps.emplace_back(grids[lev], dmap[lev], 1, 1);
+    }
+
+    MLMG mlmg(poisson);
+    mlmg.apply(GetVecOfPtrs(laps), GetVecOfPtrs(phi));
+    mlmg.getFluxes(GetVecOfArrOfPtrs(grad), GetVecOfPtrs(phi), MLMG::Location::FaceCenter);
+
+    for (int lev = 0; lev < Nlev; ++lev) {
+        // Convert to cell avg gradient
+        MultiFab gradAlias(state[lev], amrex::make_alias, idGr, AMREX_SPACEDIM);
+        average_face_to_cellcenter(gradAlias, 0, GetArrOfConstPtrs(grad[lev]));
+        gradAlias.mult(-1.0);
+#ifdef AMREX_USE_OMP
+#pragma omp parallel if (Gpu::notInLaunchRegion())
+#endif
+        for (MFIter mfi(state[lev],TilingIfNotGPU()); mfi.isValid(); ++mfi)
+        {    
+           const Box& bx = mfi.tilebox();
+           auto const& grad_a   = gradAlias.const_array(mfi);
+           auto const& gradMag  = state[lev].array(mfi,idGr+AMREX_SPACEDIM);
+           amrex::ParallelFor(bx, [=]
+           AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+           {    
+              gradMag(i,j,k) = std::sqrt(AMREX_D_TERM(  grad_a(i,j,k,0) * grad_a(i,j,k,0),
+                                                      + grad_a(i,j,k,1) * grad_a(i,j,k,1),
+                                                      + grad_a(i,j,k,2) * grad_a(i,j,k,2)));
+           });  
+        } 
+    }
+
+    // ---------------------------------------------------------------------
+    // Write the results
+    // ---------------------------------------------------------------------
+    Vector<std::string> nnames(nCompOut);
+    for (int i=0; i<nCompIn; ++i) {
+      nnames[i] = inVarNames[i];
+    }
+    nnames[idGr+0] = gradVar + "_gx";
+    nnames[idGr+1] = gradVar + "_gy";
+#if AMREX_SPACEDIM==3
+    nnames[idGr+2] = gradVar + "_gz";
+#endif
+    nnames[idGr+AMREX_SPACEDIM] = "||grad"+ gradVar+ "||";
     std::string outfile(getFileRoot(infile) + "_gt"); pp.query("outfile",outfile);
 
     Print() << "Writing new data to " << outfile << std::endl;
-
-    WritePlotFile(state,amrData,outfile,verb,nnames);
-  }
+    Vector<int> isteps(Nlev, 0);
+    Vector<IntVect> refRatios(Nlev-1,{AMREX_D_DECL(2, 2, 2)});
+    amrex::WriteMultiLevelPlotfile(outfile, Nlev, GetVecOfConstPtrs(state), nnames,
+                                   geoms, 0.0, isteps, refRatios);
   }
   amrex::Finalize();
   return 0;
