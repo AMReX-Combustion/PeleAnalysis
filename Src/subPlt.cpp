4658c301027c273d61b0fcda8b5fe2b021fe15ef
Thomas Howarth <t.howarth@itv.rwth-aachen.de>
2024-08-08 18:21:52 +0200

    subPlt fix

--- Src/subPlt.cpp
+++ Src/subPlt.cpp
@@ -99,8 +99,8 @@ main (int   argc,
         pp.getarr("box",inBox,0,nx);
         int d=BL_SPACEDIM;
         BL_ASSERT(inBox.size()==2*d);
-        subbox=Box(IntVect(D_DECL(inBox[0],inBox[1],inBox[2])),
-                   IntVect(D_DECL(inBox[d],inBox[d+1],inBox[d+2])),
+        subbox=Box(IntVect(AMREX_D_DECL(inBox[0],inBox[1],inBox[2])),
+                   IntVect(AMREX_D_DECL(inBox[d],inBox[d+1],inBox[d+2])),
                    IndexType::TheCellType());
     }
 
